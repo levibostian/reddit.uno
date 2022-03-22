252037d63dae5512e72ca6d907178d04b454e41a
Levi Bostian <levi@levi.earth>
2022-03-22 10:07:27 -0500

    build comments list with replies and depth

--- app/type/reddit/api/t1.ts
+++ app/type/reddit/api/t1.ts
@@ -5,7 +5,9 @@ export interface RedditApiT1 {
   body: string // might be markdown/encoded. Example: "[CLICK HERE TO ENTER!](http://sdqk.me/p/70Nr38mW/msi-x-pcmr-x-g-skill-white-pc-build-giveaway-qRSEyGet) (Winners' names will also be announced in this link after drawing).\n\n**Giveaway time**\n\nTime to win a Valentine-themed White PC build! We've joined forces with MSI and G.SKILL to make it happen!\u2060\n\n\u2060"
   body_html: string // ready to display html version of body 
 
-  replies: RedditApiListing<RedditApiMore | RedditApiT1>
+  replies: {
+    data: RedditApiListing<RedditApiMore | RedditApiT1>
+  } | string // "" empty string if no replies 
 
   author: string
   collapsed: boolean
